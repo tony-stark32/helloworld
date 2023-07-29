061b2541a5472b13a4cbafd8f84f006c16e58708
Wesley <wesleyjohnnalli24@gmail.com>
2023-07-29 22:57:31 +0530

    added a button

--- app/src/main/java/com/jarvis/helloworld/MainActivity.kt
+++ app/src/main/java/com/jarvis/helloworld/MainActivity.kt
@@ -2,10 +2,20 @@ package com.jarvis.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.jarvis.helloworld.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.button.setOnClickListener {
+            Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show()
+        }
     }
 }
